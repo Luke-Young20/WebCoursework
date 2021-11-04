d9728d60a04f7f3b380f170e4d8af712557358e6
Luke-Young20 <lukegyoung1@gmail.com>
2021-11-04 18:17:22 +0000

    Renamed AuthorsTableseeder
    Renamed to AuthorTableSeeder

--- database/seeders/AuthorTableSeeder.php
+++ database/seeders/AuthorTableSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Author;
-class AuthorsTableSeeder extends Seeder
+class AuthorTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -13,16 +13,6 @@ class AuthorsTableSeeder extends Seeder
      */
     public function run()
     {
-  /*      $e = new Author;
-        $e->name = 'John';
-        //e->author_id = 1;
-        $e->save();
-
-        $e = new Author;
-        $e->name = 'James';
-      //  $e->post_id = 2;
-        $e->save();
-        */
 
         $authors = Author::factory()->count(15)->create();
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-            $this->call(AuthorsTableSeeder::class);               
+            $this->call(AuthorTableSeeder::class);               
             $this->call(PostTableSeeder::class);
             $this->call(CommentTableSeeder::class);
  
