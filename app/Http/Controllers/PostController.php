a8dcc2f5444a4c3c163e10ca552d58d1f81c3ec6
Luke-Young20 <lukegyoung1@gmail.com>
2021-12-11 19:54:52 +0000

    Added a way to delete posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -119,6 +119,10 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::findOrFail($id);
+        $post->delete();
+        return redirect()->route('posts.index')->with('message','Post was deleted.');
+
+
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -14,6 +14,12 @@
         <li>Date of posting: {{$post->date_of_posting ?? 'unknown'}}</li>
     </ul>
 
-    
+    <form method="POST"
+    action="{{route('posts.destroy', ['id' => $post->id]) }}">
+    @csrf
+    @method('DELETE')
+    <button type="submit">Delete</button>
+</form>
 
+<p><a href="{{route('posts.index')}}">Back</a></p>
 @endsection
